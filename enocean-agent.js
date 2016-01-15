c2e3a2e82e736b4188726f373761390ce2369b85
gpolart <gpolart@lutraconseil.fr>
2016-01-15 15:01:18 +0100

    proof of concept enocean agent

---
+++ enocean-agent.js
@@ -0,0 +1,123 @@
+// =====================================================================
+// Copyright Gilles Polart-Donat 2015
+//
+// Domohub is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Domohub is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//    You should have received a copy of the GNU General Public License
+//    along with Domohub.  If not, see <http://www.gnu.org/licenses/>.
+// ======================================================================
+
+var debug  = require('debug')('enocean-agent');
+var enocean  = require('node-enocean')();
+var request  = require('request');
+var fs = require('fs');
+var config  = require('./config.js');
+var async  = require('async.js');
+//
+var read_active = false;
+//=========================================================================================
+// Utilities to stop process correctly
+//-----------------------------------------------------------------------------------------
+function exitHandler(options, err) {
+	debug("exitHandler   options : ", options);
+	if (options.exit) process.exit();
+}
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+// ---------------------------------------------------------------------------
+// record variable
+// ---------------------------------------------------------------------------
+function record_variable(ident, value) {
+	debug("record_variable .... ident = ", ident);
+	debug("record_variable .... value = ", value);
+
+    var headers = {
+        'User-Agent' :    'enocean-agent',
+        'Content-Type' : 'application/x-www-form-urlencoded'
+    };
+
+    var options = {
+        url : 'http://localhost:8888/api/add_value',
+        method : 'POST',
+        headers : headers,
+        form : {'ident' : ident, 'value' : value, create : 'yes' }
+    }
+
+    request(options, function(err, resp, body) {
+        debug("err = " + err);
+        debug("body = " + body);
+        // TODO test return of request
+    });
+}
+// ---------------------------------------------------------------------------
+// Get values from IPX 800 API
+// ---------------------------------------------------------------------------
+function get_values(cmd, channels, cb) {
+	debug("get_values");
+
+    if (read_active) {
+        log_message("get_values still running");
+    }
+
+    read_active = true;
+
+    var headers = {
+        'User-Agent' :    'enocean-agent',
+        'Content-Type' : 'application/json'
+    };
+
+    var options = {
+        url : 'http://'+config.enocean.ip_address+'/api/xdevices.json?cmd='+cmd,
+        method : 'GET',
+        headers : headers
+    }
+    debug("options ", options);
+
+
+
+    request(options, function(err, resp, body) {
+        if (err) {
+            cb(err, null);
+        }
+        else {
+            var data = JSON.parse(body);
+            var list = [];
+            channels.forEach(function(item, num) {
+                list.push({ label : item.key+"."+item.label, value : data[item.key] });
+            });
+            cb(null, list);
+        }
+    });
+}
+// ----------------------------------------------------------------------------------------
+// Log messages as a variable
+// ----------------------------------------------------------------------------------------
+function log_message(msg) {
+    debug("log_message ... ");
+
+    record_variable("enocean.logs", msg);
+}
+//=========================================================================================
+// Main server initialize
+//-----------------------------------------------------------------------------------------
+function start(options) {
+	debug("========  enocean-agent starting ==========");
+	log_message(" enocean-agent starting ...");
+
+    debug("options = ", options);
+    enocean.listen(options.device);
+
+    enocean.on("data", function(data) {
+        debug("data = ", data);
+    });
+
+}
+
+start(config.enocean);
